6f13be4faa49addba9852f7d4703d406616ea346
AmbadasAdhav <ambadasadhav@yahoo.com>
2017-07-12 21:32:28 +0530

    commit for annotation

---
+++ src/test/java/com/info/TestRunner/Grouping.java
@@ -0,0 +1,52 @@
+package com.info.TestRunner;
+
+import org.testng.annotations.Test;
+
+public class Grouping {
+	
+	
+		  @Test (groups = { "Sanity" ,"Regression" },priority=1)
+		  public void login() {
+	
+			  System.out.println("Login Execute Successfully");
+		 
+		  }
+		 
+		  @Test (groups = { "Sanity" ,"Regression" },priority=10)
+		  public void logout() {
+		 
+			  System.out.println("Logout Execute Successfully");
+		 
+		  }
+		 
+		  @Test (groups = { "Sanity"},priority=2)
+		  public void search() {
+		 
+			  System.out.println("Search Execute Successfully");		 
+		  }
+		 
+		  @Test (groups = { "Sanity" ,"Regression" },priority=3)
+		  public void advancedSearch() {
+		 
+			  System.out.println("advancedSearch Execute Successfully");
+		 
+		  }
+		 
+		  @Test (groups = {"Regression" },priority=4)
+		  public void billPayment() {
+		 
+			  System.out.println("billPayment Execute Successfully");
+		 
+		  }
+		  
+		 
+		  @Test (groups = {"Regression" },priority=5)	 
+		  public void prepaidRecharge() {
+		 
+			  System.out.println("prepaidRecharge Execute Successfully");
+		 
+		  }
+		 
+		}
+
+
